1a877151a88a10eeb9165ee3062b0b0103c97c97
kagfupanda <kagfupanda@gmail.com>
2021-07-05 19:51:03 -0700

    initial commit

---
+++ overview/src/HelloGoodbye.java
@@ -0,0 +1,10 @@
+
+public class HelloGoodbye {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hello %s and %s \n", args[0], args[1]);
+		System.out.printf("Goodbye %s and %s \n", args[1], args[0]);
+	}
+
+}
